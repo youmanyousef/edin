0725b13ceee8262853a5c535d76524e34abaca28
youmanyousef <44135498+youmanyousef@users.noreply.github.com>
2021-01-01 12:27:26 -0800

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'rank',
 	description: "This has absolutely no purpose other than inside memes.",
 	execute(message,args){
-		var randomJ = Math.floor(Math.random() * 23);  
+		var randomJ = Math.floor((Math.random() * 24)+1);  
 		message.channel.send({files: ["./images/rank/" + randomJ + ".gif"]});
 		
 	}
